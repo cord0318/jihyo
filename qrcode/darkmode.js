a7720c6f72ef2e70e0f98aaea95b3e0cfaa7132c
cord0318@gmail.com <jihyo@jeongjihyoui-MacBookPro.local>
2022-02-14 11:28:06 +0900

    😎 pyramid 페이지의 enter키 감지 생성

---
+++ qrcode/darkmode.js
@@ -0,0 +1,9 @@
+var darkmode = false;
+
+function clickDarkMode() {
+    if (darkmode) { // if darkmode is on
+        console.log("Darkmode is on");
+    } else { // if darkmode is off
+        console.log("Darkmode is off");
+    }
+}
